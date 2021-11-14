ef157b83c9c7906714dc7c52752b9afeaf29857d
Ahmed Dawuda <kofidahmed@gmail.com>
2021-11-14 00:18:16 +0000

    added helpers to build validation error

---
+++ src/helpers.ts
@@ -0,0 +1,24 @@
+import {Error} from 'mongoose';
+import { HTTP_ERROR, HTTP_CODES } from './types';
+
+/**
+ * Transforms mongoose database validation error
+ *
+ * @param error mongoose error
+ * @param data other data to send
+ * @returns {HTTP_ERROR}
+ */
+export const buildModelValidationErrors = (error: Error.ValidationError, data: any = null): HTTP_ERROR => {
+    const messages: any = {};
+
+    for (const key of Object.keys(error.errors)) {
+        messages[key] = error.errors[key].message;
+    }
+
+    return {
+        status: HTTP_CODES.UNPROCESSABLE_ENTITY,
+        message: 'The given data was invalid',
+        errors: messages,
+        data
+    };
+}
