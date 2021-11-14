8c1b76581b18cade8721febb823ae8cbf62dbe74
Ahmed Dawuda <kofidahmed@gmail.com>
2021-11-14 00:40:05 +0000

    added enhancements to app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 require('dotenv').config();
+const cors = require('cors');
 import express, { Application, Request, Response } from 'express';
-import routes from './routes';
-
+import router from './router';
+import database from './database';
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(router);
 
-app.listen(process.env.APP_PORT, () => console.log(`Listening on port ${process.env.APP_PORT}`));
+database.connect();
+
+app.listen(process.env.APP_PORT, (): void => console.log(`Listening on port ${process.env.APP_PORT}`));
+
+/**
+ * Declare new properties on global express namespace
+ */
+declare global {
+  namespace Express {
+    interface Request {
+      auth?: any
+    }
+  }
+}
