ddd37e40c84325cbee3ac56ba59f0437ef9ce8d8
Ahmed Dawuda <kofidahmed@gmail.com>
2021-11-14 13:03:02 +0000

    added index and store

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from 'express';
-import { RouteAction } from '../types';
+import { HTTP_CODES, RouteAction } from '../types';
+
+import PostService from './../services/post';
 
 /**
  * List all blog posts
@@ -8,7 +10,9 @@ import { RouteAction } from '../types';
  * @param {Response} response response object
  */
 export const index: RouteAction = async (request: Request, response: Response) => {
-  response.json({auth: !!request.auth});
+  PostService(request.authUser).index({...request.query, ...request.params})
+    .then((data: any) => response.status(HTTP_CODES.CREATED).json(data))
+    .catch((error: any) => response.status(HTTP_CODES.UNPROCESSABLE_ENTITY).json(error));
 }
 
 /**
@@ -18,5 +22,10 @@ export const index: RouteAction = async (request: Request, response: Response) =
  * @param {Response} response response object
  */
 export const store: RouteAction = async (request: Request, response: Response) => {
-  response.json(request.auth);
+  const { title, content } = request.body;
+  const createdBy = request.authUser?._id;
+
+  PostService(request.authUser).store({ title, content, createdBy })
+    .then(post => response.status(HTTP_CODES.CREATED).json(post))
+    .catch(error => response.status(HTTP_CODES.UNPROCESSABLE_ENTITY).json(error));
 }
